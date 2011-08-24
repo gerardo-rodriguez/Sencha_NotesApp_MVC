4923446160c437694cb352139f0f8f63839c0f22
Gerardo Rodriguez <grodriguez@cmdagency.com>
2011-08-24 10:58:09 -0700

    Added link to tutorial

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// THis app was created following this tutorial: http://css.dzone.com/articles/writing-sencha-touch-mvc
 // This registers the name of the application
 Ext.regApplication({
 	name: "NotesApp",
